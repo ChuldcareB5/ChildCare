584b134bf5af88a8e864ecd27a352c5db0271f9b
ChuldcareB5 <ashaash663@gmail.com>
2020-01-22 12:32:51 +0530

    Removed system files.

--- Source Code/03 Presentation/ChildCare.MonitoringSystem.Web/Controllers/HomeController.cs
+++ Source Code/03 Presentation/ChildCare.MonitoringSystem.Web/Controllers/HomeController.cs
@@ -10,31 +10,113 @@ namespace ChildCare.MonitoringSystem.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
+        //public IActionResult About()
+        //{
+        //    ViewData["Message"] = "Your application description page.";
 
-            return View();
-        }
+        //    return View();
+        //}
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
+		public IActionResult Contact()
+		{
+			//ViewData["Message"] = "Your contact page.";
 
-            return View();
-        }
+			return View("Contact");
+		}
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+		public IActionResult Mainpage()
+		{
+			return View("HomePage");
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Student()
+		{
+			return View("Student");
+		}
+
+		public IActionResult Camera()
+		{
+			return View("Camera");
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult About()
+		{
+			ViewData["Message"] = "Your application description page.";
+
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+		public IActionResult Dashboard()
+		{
+			return View("admin");
+		}
+		public IActionResult Form()
+		{
+			return View("StudentRegistration");
+		}
+		public IActionResult Form1()
+		{
+			return View("TeacherRegistration");
+		}
+		public IActionResult Calender()
+		{
+			return View("Calender");
+		}
+		public IActionResult Calender1()
+		{
+			return View("Calender1");
+		}
+		public IActionResult CamaraView()
+		{
+			return View("CamaraView");
+		}
+		public IActionResult BusTracking()
+		{
+			return View("BusTracking");
+		}
+		public IActionResult BusSchedule()
+		{
+			return View("BusSchedule");
+		}
+		public IActionResult StudentLocation()
+		{
+			return View("StudentLocation");
+		}
+		public IActionResult StudentDetails()
+		{
+			return View("StudentView");
+		}
+		public IActionResult StudentClassDetails()
+		{
+			return View("StudentDetails");
+		}
+		public IActionResult ParentAccount()
+		{
+			return View("ParentAccount");
+		}
+		public IActionResult StudentCameraView()
+		{
+			return View("CameraView");
+		}
+		public IActionResult Onclicks()
+		{
+			return View("Onclicks");
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
